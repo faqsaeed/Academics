e882ee1d504604606cf6444eb77355c0a0a6295f
Faiq Saeed <143216504+faqsaeed@users.noreply.github.com>
2024-05-10 02:30:37 +0500

    Update Social Media.cpp

--- OOP/Project(Social Media App)/Social Media.cpp
+++ OOP/Project(Social Media App)/Social Media.cpp
@@ -4,11 +4,11 @@
 #include <ctime>
 using namespace std;
 
+const int MAX = 10;
 class Uid {
 private:
 	string id;
 	static int post;
-	static int comment;
 	static int page;
 	static int user;
 public:
@@ -17,7 +17,7 @@ public:
 		return id;
 	}
 
-	Uid(string initial) //initiaiser is used to differentiate between pot, comments etc
+	Uid(string initial) //initiaiser is used to differentiate between Uid of pot, comments etc
 	{
 		
 		int val = rand() % 100;
@@ -37,15 +37,59 @@ public:
 			id = initial + to_string(page);
 			page++;
 		}
-		else if (initial == "C")
+	}
+};
+
+class Post: public Uid{
+private:
+	time_t t;// time add krna hai bc fujtionality requires so :(
+	string text;
+	string* likes = new string[10]; // might replace it with user ki array ;<
+public:
+	Post(string Text) :Uid("P"), text(Text) {}
+
+	string getText() const
+	{
+		return text;
+	}
+	 
+	~Post()
+	{
+		delete[] likes;
+		likes = nullptr;
+	}
+};
+
+class User : public Uid {
+private:
+	User** friends = new User * [MAX];
+	string name;
+	Post** post = new Post * [MAX];
+	int posts = 0;
+public:
+	User(string Name) : Uid("U"), name(Name) {}
+
+	void createPost(string text)
+	{
+		post[posts] = new Post(text);
+		posts++;
+	}
+
+	string getName() const
+	{
+		return name;
+	}
+
+	void printPost() const
+	{
+		for (int i = 0; i < posts; ++i)
 		{
-			id = initial + to_string(comment);
-			comment++;
+			cout << post[i]->getId() << endl << post[i]->getText() << endl;
 		}
 	}
 };
 
-int Uid::comment = 0;
+
 int Uid::page = 0;
 int Uid::user = 0;
 int Uid::post = 0;
@@ -53,8 +97,13 @@ int Uid::post = 0;
 
 int main()
 {
-	Uid u1("U");
-	Uid u2("U");
-	Uid p1("P");
-	cout << u1.getId() << " " << u2.getId() << " " << p1.getId();
+	User u1("Faiq");
+	User u2("Ali");
+	cout << u1.getId() << endl << u2.getId() << endl;
+	u1.createPost("General Asim Munir");
+	u2.createPost("Pakitan Zindabad");
+	u1.createPost("PTI lai loi");
+	u2.createPost("Pak Debate Forum");
+	u1.printPost();
+	u2.printPost();
 }
